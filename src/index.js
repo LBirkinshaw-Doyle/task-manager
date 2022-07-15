e83a3949438c14d81b3d263038113385202bd85f
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-07-15 10:27:27 +0100

    First commit initialize project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './normalize.css';
+import './style.css';
+//import {thing1, thing2} from './module1.js';
+//import * as Module2 from './module2.js';
+
+console.log('testing 3');
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src", "index.html")
+		})
+	],
+    devServer: {
+        static: './dist',
+    },
+    optimisation: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
