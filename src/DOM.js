6651daa22c80c2e3c2ba373402b3f906b3be4fee
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-07-20 13:46:15 +0100

    start task list dropdown mobile

--- src/DOM.js
+++ src/DOM.js
@@ -8,6 +8,24 @@ export function createStatic () {
 
     title.textContent = 'Tsk Mng';
 
+    const navBar = document.createElement('nav');
+    const displaySelector = document.createElement('a');
+    const treeImage =document.createElement('span');
+    const taskListHeader = document.createElement('h3');
+    const taskList = document.createElement('ul');
+
+    taskListHeader.id = 'task-list-header';
+    taskListHeader.textContent = 'Tasks:';
+    taskList.id = "task-list";
+    taskListHeader.classList.add('task');
+    taskList.classList.add('task');
+    treeImage.id = 'tree-image';
+    displaySelector.href = "javascript:void(0)"
+
+    displaySelector.appendChild(treeImage);
+    navBar.append(displaySelector, taskListHeader, taskList);
+    header.append(navBar);
+
     titleContainer.append(logoImage, title);
     header.append(titleContainer);
     content.append(header);
